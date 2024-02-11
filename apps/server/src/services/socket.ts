c6be9177f261c8292f3ea361c342971897958181
Ankur Semle <semleankur@gmail.com>
2024-02-11 23:18:34 +0530

    add event:message in socket.ts

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,28 @@
+    import { Server } from "socket.io";
+
+    class SocketService {
+        private _io: Server;
+
+        constructor(){
+            console.log("Init Socket Server...")
+            this._io = new Server();
+        }
+
+        public initListeners() {
+            const io = this.io;
+            console.log("Initialize socket listeners...")
+            
+            io.on('connect', socket => {
+                console.log(`New Socket Connected: ${socket.id}`);
+                socket.on("event:message", async ({ message }: { message: string}) => {
+                    console.log('New Message Rec.', message);
+                })
+            })
+        }
+
+        get io() {
+            return this._io;
+        }
+    }
+
+    export default SocketService;
