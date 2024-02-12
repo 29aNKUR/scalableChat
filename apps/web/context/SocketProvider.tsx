2c1e38bcd8eac593c88570211dd67cee12937eb4
Ankur Semle <semleankur@gmail.com>
2024-02-12 23:49:56 +0530

    correct commit

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,16 +1,33 @@
 //context should execute on client and not on server
 'use-client'
 
-import React from "react"
-
+import React, { useCallback, useEffect } from "react"
+import { io } from "socket.io-client"
 interface SocketProviderProps {
     children?: React.ReactNode 
 }
 
-const SocketContext = React.createContext(null);
+interface ISocketContext {
+    sendMessage: (msg : string) => any;
+}
+
+const SocketContext = React.createContext<ISocketContext | null>(null);
 
 //FC-functional component
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+
+    const sendMessage: ISocketContext['sendMessage'] = useCallback((msg) => {
+        console.log("send msg", msg)
+    },[])
+
+    useEffect(() => {
+        const _socket = io("http://localhost:8000");
+
+        return () => {
+            _socket.disconnect();
+        }
+    },[])
+
     return (
         <SocketContext.Provider value={null}>
             {children}
